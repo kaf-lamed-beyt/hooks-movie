f30d8c42af5d43bb20f609479d54ea7497d32b3b
Caleb335 <belac335@gmail.com>
2020-05-07 15:45:08 +0100

    configuring babel presets

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div className="app">
+            <h1>Hello movies 😆 </h1>
+        </div>
+    )
+}
+
+export default App
+
