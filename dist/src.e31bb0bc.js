90affd9418112a63d1eeea97debb394fd6cc62a7
Caleb335 <belac335@gmail.com>
2020-05-16 01:34:42 +0100

    fixed movie component

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -28623,7 +28623,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40145" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45377" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
