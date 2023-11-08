fad60ada1679b8a2e6d9f9dce059e5011ee384b6
alubọsa <48127194+kaf-lamed-beyt@users.noreply.github.com>
2023-11-08 07:42:04 +0100

    testing

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom'
 import './scss/app.scss'
 import App from './components/App'
 
+const NEXT_PUBLIC_KEY=4a3b711b
+
 ReactDOM.render(<App />, document.getElementById('root'))
